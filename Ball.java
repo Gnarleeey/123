8bb44bfd4bac2d00b3ef083d393a7f6e69616b61
Gnarleeey <Gnarley@yahoo.com>
2019-01-10 14:58:07 +0800

    Gallery

---
+++ Ball.java
@@ -0,0 +1,14 @@
+public class Ball{
+	
+	public float ballRadius = 10;
+	public float ballX = ballRadius + 50;
+	public float ballY = ballRadius + 20;
+	public float ballSpeedX = 3;
+	public float ballSpeedY = 2;
+
+
+	public balls(){
+
+	
+	}
+}
